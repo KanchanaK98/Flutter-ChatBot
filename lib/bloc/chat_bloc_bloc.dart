d26379ea0983cea2edb74d671e14574ff6ae0538
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2024-03-06 12:57:23 +0530

    modified chat bloc

--- lib/bloc/chat_bloc_bloc.dart
+++ lib/bloc/chat_bloc_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:chatbot_geminiai/models/chat_message_model.dart';
+import 'package:chatbot_geminiai/repos/chat_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_bloc_event.dart';
@@ -13,9 +14,11 @@ class ChatBlocBloc extends Bloc<ChatBlocEvent, ChatBlocState> {
     
   }
 List<ChatMessageModel> messages = [];
-  FutureOr<void> chatGenerateNewTextMessageEvent(ChatGenerateNewTextMessageEvent event, Emitter<ChatBlocState> emit) {
+  FutureOr<void> chatGenerateNewTextMessageEvent(ChatGenerateNewTextMessageEvent event, Emitter<ChatBlocState> emit) async {
     messages.add(ChatMessageModel(role: "user", parts: [
       ChatPartModel(text: event.inputMessage)
-    ]))
+    ]));
+    emit(ChatSuccessState(messages: messages));
+    await ChatRepo.chatTextGenerationRepo(messages);
   }
 }
