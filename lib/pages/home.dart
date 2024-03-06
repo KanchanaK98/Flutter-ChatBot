c0d4145221702c6a09de1922475c26661e8b51ee
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2024-03-06 11:53:58 +0530

    change home UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,8 +20,15 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             Container(
-              height: 120,
-              color: Colors.red,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 100,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Chat App",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,fontFamily: 'MadimiOne'),)
+                ],
+              ),
             ),
             Expanded(child: ListView()),
             Container(padding: EdgeInsets.symmetric(vertical: 12,horizontal: 12),
@@ -32,7 +39,7 @@ class _HomePageState extends State<HomePage> {
                 decoration: InputDecoration(labelText: 'Chatting...',border: OutlineInputBorder(borderRadius: BorderRadius.circular(120)),focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 176, 237, 11)),borderRadius: BorderRadius.circular(120)),fillColor: Colors.white10,filled: true),
               )),
               const SizedBox(width: 10),
-              CircleAvatar(radius: 28,backgroundColor: Colors.white,child: CircleAvatar(radius: 25,backgroundColor: Colors.blue,child: Center(child: Icon(Icons.send,color: Colors.white,),),))
+              const CircleAvatar(radius: 28,backgroundColor: Colors.white,child: CircleAvatar(radius: 25,backgroundColor: Colors.blue,child: Center(child: Icon(Icons.send,color: Colors.white,),),))
             ],),)
           ],
         ),
